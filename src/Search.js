6333498c7908f9129801fd2faa39c09e818dcc7a
shuaizhao1988 <69214951+shuaizhao1988@users.noreply.github.com>
2020-10-15 16:08:48 -0400

    deleted the queryallByAttribute

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { queryAllByAttribute } from "@testing-library/dom";
 
 export default function Search() {
   const [city, setCity] = useState("");
